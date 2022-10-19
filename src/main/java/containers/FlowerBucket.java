c0595e32ad7f413174508b678c183529ef3f656f
Andriy Romanyuk <vandzhuraandrew@gmail.com>
2022-10-19 16:55:41 +0300

    Initial commit

---
+++ src/main/java/containers/FlowerBucket.java
@@ -0,0 +1,18 @@
+package containers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowerBucket {
+    private List<FlowerPack> bucket = new ArrayList<>();
+    public void add(FlowerPack pack) {
+        bucket.add(pack);
+    }
+    public double getPrice() {
+        double result = 0;
+        for (FlowerPack pack : bucket) {
+            result += pack.getPrice();
+        }
+        return result;
+    }
+}
